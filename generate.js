178100daf7df54cb19c5a7bd270d923544bb170d
Amnish Arora <amnish.arora@ontario.ca>
2023-02-13 14:40:36 -0500

    Refactor generate.js

--- generate.js
+++ generate.js
@@ -1,17 +1,23 @@
+const { faker } = require("@faker-js/faker");
+const _ = require("lodash");
+
 module.exports = function() {
- const { faker } = require("@faker-js/faker");
- const _ = require("lodash");
 
  return {
-    users: _.times(50, function(i) {
-        return {
-            id: i,
-            userType: faker.helpers.arrayElement(["admin", "viewer"]),
-            name: faker.name.firstName(),
-            email: faker.internet.email(this.name),
-            pwd: faker.internet.password(10, true),
-            dob: faker.date.birthdate({ min: 18, max: 65, mode: 'age' })
-        }
-    })
+    users: _.times(50, generateUser)
  };
+}
+
+/*
+ Generator Functions
+*/
+function generateUser(id) {
+    return {
+        id: id,
+        userType: faker.helpers.arrayElement(["admin", "viewer"]),
+        name: faker.name.firstName(),
+        email: faker.internet.email(this.name),
+        pwd: faker.internet.password(10, true),
+        dob: faker.date.birthdate({ min: 18, max: 65, mode: 'age' })
+    }
 }
